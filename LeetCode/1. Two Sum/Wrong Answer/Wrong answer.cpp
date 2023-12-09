f89c4dc49db1186f478e73ad6d6d73489d1a826f
KarimanAmmar <76701636+KarimanAmmar@users.noreply.github.com>
2023-12-09 02:39:51 +0200

    Add files via upload

---
+++ LeetCode/1. Two Sum/Wrong Answer/Wrong answer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+    int size = sizeof(nums)/sizeof(nums[0]);
+      for(int i =0 ; i<size ; i++)
+      {
+        for(int j =i+1 ; j<size ; j++)
+        {
+            if(nums[i] + nums[j] == target)
+            {
+               return {i,j};
+            }
+        }
+
+      }
+      return {-1};
+    }
+};
